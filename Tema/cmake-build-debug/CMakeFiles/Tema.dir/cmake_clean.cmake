a8cd36431d433c98fa70ff3c1f7ccc15713dc683
Dionisie Hadirca <79158638+JohnPs1na@users.noreply.github.com>
2021-03-04 15:46:37 +0200

    Add files via upload

---
+++ Tema/cmake-build-debug/CMakeFiles/Tema.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tema.dir/sortari.cpp.obj"
+  "Tema.exe"
+  "Tema.exe.manifest"
+  "Tema.pdb"
+  "libTema.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tema.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
